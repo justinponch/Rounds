9b52592c6c47a4db42d838ebd0d0bf000e7e0171
Justin <justinpontrella@gmail.com>
2020-08-11 01:06:37 -0400

    first

---
+++ rounds.js
@@ -0,0 +1,81 @@
+const { stdout } = require("process");
+
+const readline = require("readline")
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("Begin?").then(res => {
+    
+})
+
+let stuff = {
+    29: {
+        conductivity = {
+            p1: "",
+            p2: "",
+        },
+        P2sandFilter = {
+            on: null,
+            in: "",
+            out: "",
+            backwash: "",
+        },
+        P3sandFilter = {
+            on: null,
+            in: "",
+            out: "",
+            backwash: "",
+        },
+        Gens = {
+            gen1:[],
+            gen2:[],
+            gen3:[],
+            gen4:[],
+            gen5:[],
+        }
+    },
+    30: {
+        P2makeup: "",
+        P3makeup: "",
+        CT2:{
+            t1: null,
+            t2: null,
+            t3: null,
+            t4: null,
+            t5: null,
+            t6: null,
+        },
+        CT3:{
+            t1: null,
+            t2: null,
+            t3: null,
+            t4: null,
+        }
+    },
+    2: {
+        MDF:{
+            PumpA: null,
+            PumpB: null,
+        }
+    },
+    b1: {
+        batteryA: null,
+        batterB: null,
+        switchA: null,
+        switchB: null,
+        UPSa: null,
+        UPSb: null,
+    },
+    b2:{
+        comp: {
+            recipA: null,
+            recipB: null,
+            recipC: null,
+        }
+    },
+    b3:{
+        grease:[]
+    }
+}
