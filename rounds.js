44bf245f2092228d0b1f2d0db05404b02f749374
alvin lubetkin <alvin@terminal.co>
2020-08-11 01:36:13 -0400

    response being taken

--- rounds.js
+++ rounds.js
@@ -1,81 +1,10 @@
-const { stdout } = require("process");
-
-const readline = require("readline")
+const readline = require("readline");
+const { data } = require("./roundsData");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.question("Begin?").then(res => {
-    
-})
-
-let stuff = {
-    29: {
-        conductivity = {
-            p1: "",
-            p2: "",
-        },
-        P2sandFilter = {
-            on: null,
-            in: "",
-            out: "",
-            backwash: "",
-        },
-        P3sandFilter = {
-            on: null,
-            in: "",
-            out: "",
-            backwash: "",
-        },
-        Gens = {
-            gen1:[],
-            gen2:[],
-            gen3:[],
-            gen4:[],
-            gen5:[],
-        }
-    },
-    30: {
-        P2makeup: "",
-        P3makeup: "",
-        CT2:{
-            t1: null,
-            t2: null,
-            t3: null,
-            t4: null,
-            t5: null,
-            t6: null,
-        },
-        CT3:{
-            t1: null,
-            t2: null,
-            t3: null,
-            t4: null,
-        }
-    },
-    2: {
-        MDF:{
-            PumpA: null,
-            PumpB: null,
-        }
-    },
-    b1: {
-        batteryA: null,
-        batterB: null,
-        switchA: null,
-        switchB: null,
-        UPSa: null,
-        UPSb: null,
-    },
-    b2:{
-        comp: {
-            recipA: null,
-            recipB: null,
-            recipC: null,
-        }
-    },
-    b3:{
-        grease:[]
-    }
-}
+rl.question("Begin?", (res) => {
+  console.log("response", res);
+});
---
+++ roundsData.js
@@ -0,0 +1,70 @@
+const stuff = {
+  29: {
+    conductivity: {
+      p1: "",
+      p2: "",
+    },
+    P2sandFilter: {
+      on: null,
+      in: "",
+      out: "",
+      backwash: "",
+    },
+    P3sandFilter: {
+      on: null,
+      in: "",
+      out: "",
+      backwash: "",
+    },
+    Gens: {
+      gen1: [],
+      gen2: [],
+      gen3: [],
+      gen4: [],
+      gen5: [],
+    },
+  },
+  30: {
+    P2makeup: "",
+    P3makeup: "",
+    CT2: {
+      t1: null,
+      t2: null,
+      t3: null,
+      t4: null,
+      t5: null,
+      t6: null,
+    },
+    CT3: {
+      t1: null,
+      t2: null,
+      t3: null,
+      t4: null,
+    },
+  },
+  2: {
+    MDF: {
+      PumpA: null,
+      PumpB: null,
+    },
+  },
+  b1: {
+    batteryA: null,
+    batterB: null,
+    switchA: null,
+    switchB: null,
+    UPSa: null,
+    UPSb: null,
+  },
+  b2: {
+    comp: {
+      recipA: null,
+      recipB: null,
+      recipC: null,
+    },
+  },
+  b3: {
+    grease: [],
+  },
+};
+exports.data = stuff;
